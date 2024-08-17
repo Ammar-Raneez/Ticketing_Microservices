908a3a0d42ca21efd6087375add8be50628036c7
Ammar-Raneez <ammarraneez@gmail.com>
2024-08-17 12:06:33 +0530

    Setup ingress controller

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,8 +4,11 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
-const PORT = 3000;
+app.get('/api/users/current-user', (_, res) => {
+  res.send("Hi There");
+})
 
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 })
