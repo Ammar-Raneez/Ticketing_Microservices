5bb9fc4a06f3193dad7caef7f09a94bb1e524352
Ammar-Raneez <ammarraneez@gmail.com>
2024-08-20 20:31:58 +0530

    Add mongoose and create user

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 
+import { Password } from '../services/password';
+
 // User properties
 interface UserAttrs {
   email: string;
@@ -22,6 +24,17 @@ const userSchema = new mongoose.Schema({
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 }
+
+userSchema.pre('save', async function (done) {
+  // "this" is preserved only in regular functions, not arrow functions
+  // Therefore, in this case this refers to the current user document
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+
+  done();
+});
 
 // Enfore user typing
 export const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
