3aa28280be4ec31181aa222539f964ac7bf07297
Ammar-Raneez <ammarraneez@gmail.com>
2024-09-30 15:27:01 +0530

    Create order cancelled listener

---
+++ tickets/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,40 @@
+import { Message } from "node-nats-streaming";
+
+import {
+  Listener,
+  OrderCancelledEvent,
+  Subjects,
+} from "@ar-personal/tickets-common";
+
+import { queueGroupName } from "./queue-group-name";
+import { Ticket } from "../../models/Ticket";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
+
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+  subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
+
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCancelledEvent["data"], msg: Message) {
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    if (!ticket) throw new Error("Ticket not found");
+
+    // A cancelled ticket won't have an order
+    ticket.set({ orderId: undefined });
+
+    await ticket.save();
+
+    // Publish ticket update event for orders service to sync
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      price: ticket.price,
+      title: ticket.title,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+      version: ticket.version,
+    });
+
+    msg.ack();
+  }
+}
