acb45800b781c130aabf6196e34f918a14b28bf4
Ammar-Raneez <ammarraneez@gmail.com>
2024-10-02 14:02:35 +0530

    Add stripe secret config

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -8,6 +8,8 @@ import { OrderCancelledListener } from "./events/listeners/order-cancelled-liste
 const start = async () => {
   if (!process.env.JWT_KEY) throw new Error("JWT_KEY must be defined");
 
+  if (!process.env.STRIPE_KEY) throw new Error("STRIPE_KEY must be defined");
+
   if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined");
 
   if (!process.env.NATS_CLUSTER_ID)
